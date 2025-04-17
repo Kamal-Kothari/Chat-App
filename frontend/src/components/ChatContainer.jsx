e4a71cce28d810ab333d5a2fd5a8aa1b21be427e
Kamal <kamallogin8@gmail.com>
2025-04-17 10:36:55 +0530

    chat header

--- frontend/src/components/ChatContainer.jsx
+++ frontend/src/components/ChatContainer.jsx
@@ -1,8 +1,32 @@
 import React from 'react'
+import { useChatStore } from '../store/useChatStore.js'
+import { useEffect } from 'react'
+import { Loader } from 'lucide-react'
+import ChatHeader from './ChatHeader.jsx'
+import MessageInput from './MessageInput.jsx'
 
 const ChatContainer = () => {
+  const { selectedUser, getMessages, messages, isMessagesLoading } = useChatStore();
+
+  useEffect(() => {
+    if (selectedUser) {
+      getMessages(selectedUser._id);
+    }
+  },[selectedUser._id, getMessages])
+
+  if(isMessagesLoading) {
+    return (
+      <div className='flex flex-1 justify-center items-center h-full'>
+        <Loader className="size-10 animate-spin" />
+      </div>
+    )
+  }
   return (
-    <div>ChatContainer</div>
+    <div className='flex flex-col flex-1 overflow-auto'>
+      <ChatHeader />
+      <p>messages ...</p>
+      <MessageInput />
+    </div>
   )
 }
 
