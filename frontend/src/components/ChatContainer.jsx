37a931686f5f08dc1a462d11f1faf96a39a8b34b
Kamal <kamallogin8@gmail.com>
2025-04-23 12:51:59 +0530

    show messages

--- frontend/src/components/ChatContainer.jsx
+++ frontend/src/components/ChatContainer.jsx
@@ -5,9 +5,12 @@ import { Loader } from 'lucide-react'
 import ChatHeader from './ChatHeader.jsx'
 import MessageInput from './MessageInput.jsx'
 import MessageSkeleton from './skeleton/MessageSkeleton.jsx'
+import { formatMessageTime } from '../lib/utils.js'
+import { useAuthStore } from '../store/useAuthStore.js'
 
 const ChatContainer = () => {
   const { selectedUser, getMessages, messages, isMessagesLoading } = useChatStore();
+  const { authUser } = useAuthStore();
 
   useEffect(() => {
     if (selectedUser) {
@@ -27,7 +30,42 @@ const ChatContainer = () => {
   return (
     <div className='flex flex-col flex-1 overflow-auto'>
       <ChatHeader />
-      <p>messages ...</p>
+      <div className="flex-1 overflow-y-auto p-4 space-y-4">
+        {messages.map((message) => (
+          <div
+            key={message._id}
+            className={`chat ${message.senderId === authUser._id ? "chat-end" : "chat-start"}`}
+          >
+            <div className=" chat-image avatar">
+              <div className="size-10 rounded-full border">
+                <img
+                  src={
+                    message.senderId === authUser._id
+                      ? authUser.profilePic || "/avatar.png"
+                      : selectedUser.profilePic || "/avatar.png"
+                  }
+                  alt="profile pic"
+                />
+              </div>
+            </div>
+            <div className="chat-header mb-1">
+              <time className="text-xs opacity-50 ml-1">
+                {formatMessageTime(message.createdAt)}
+              </time>
+            </div>
+            <div className="chat-bubble flex flex-col">
+              {message.image && (
+                <img
+                  src={message.image}
+                  alt="Attachment"
+                  className="sm:max-w-[200px] rounded-md mb-2"
+                />
+              )}
+              {message.text && <p>{message.text}</p>}
+            </div>
+          </div>
+        ))}
+      </div>
       <MessageInput />
     </div>
   )
