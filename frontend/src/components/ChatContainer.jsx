c2098d8b7a6246740f21f81ead5121a576acda7e
Kamal <kamallogin8@gmail.com>
2025-04-30 20:21:16 +0530

    user seed. scroll to empty div at botto,

--- frontend/src/components/ChatContainer.jsx
+++ frontend/src/components/ChatContainer.jsx
@@ -46,7 +46,7 @@ const ChatContainer = () => {
           <div
             key={message._id}
             className={`chat ${message.senderId === authUser._id ? "chat-end" : "chat-start"}`}
-            ref={messageEndRef}
+
           >
             <div className=" chat-image avatar">
               <div className="size-10 rounded-full border">
@@ -77,6 +77,7 @@ const ChatContainer = () => {
             </div>
           </div>
         ))}
+        <div ref={messageEndRef}></div>
       </div>
       <MessageInput />
     </div>
