66b69887a2aee2e4199b8906f0106b8224795ed7
Kamal <kamallogin8@gmail.com>
2025-05-02 10:50:19 +0530

    deployment setup

--- backend/src/index.js
+++ backend/src/index.js
@@ -5,11 +5,15 @@ import dotenv from 'dotenv';
 import cors from 'cors'; // Middleware to enable CORS (Cross-Origin Resource Sharing)
 dotenv.config(); // Load environment variables from .env file 
 import cookieParser from 'cookie-parser'; // Middleware to parse cookies
+import path from 'path'; // Node.js module to work with file and directory paths
 
 import { connectDB } from './lib/db.js'; // Import the connectDB function
 
 // const app = express();
 import {app, server} from './lib/socket.js'; // Import the app and server from socket.js
+
+const port = process.env.PORT || 5001; // Use the port from environment variables or default to 5001
+const __dirname = path.resolve(); // Get the current directory name
 
 app.use(express.json()); // Middleware to parse JSON request bodies
 // app.use(express.urlencoded({ extended: true })); // Middleware to parse URL-encoded request bodies
@@ -23,7 +27,13 @@ app.use(cors({
 app.use('/api/auth',authRoutes);
 app.use('/api/messages',messageRoutes);
 
-const port = process.env.PORT || 5001; // Use the port from environment variables or default to 5001
+if(process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '../frontend/dist'))); // Serve static files from the frontend build directory
+  app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '../frontend/dist/index.html')); // Serve the index.html file for all other routes
+  });
+}
+
 server.listen(port , () => {
   console.log('Server is running on port : '+ port);
   connectDB(); // Call the connectDB function to connect to MongoDB
